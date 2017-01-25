0b7ee2cdc28e845f7b488e0393953309c2d778d3
Toshirox <grimjoww91@gmail.com>
2017-01-25 23:35:00 +0100

    Ajout des statistiques pour les arbres généraux.

--- src/recursive/Statistiques.java
+++ src/recursive/Statistiques.java
@@ -1,5 +1,8 @@
 package recursive;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Statistiques {
 
 	public static int profondeur(Noeud arbre){
@@ -12,6 +15,54 @@ public class Statistiques {
 			profFin = 1;
 
 		return profFin;
+	}
+	
+	public static int profondeurGenTree(GeneralTree tree){
+		int profFin = 0;
+		
+		if(tree.getChildren().isEmpty())
+			return 1;
+		else {
+			List<GeneralTree> l = tree.getChildren();
+			List<Integer> prof = new LinkedList<>();
+			for(GeneralTree gt : l){
+				profFin = (1+profGenTreeChild(gt));
+				prof.add(profFin);
+			}
+			
+			int base = 0;
+			for(Integer v : prof){
+				base = Math.max(base, v);
+			}
+			return base;
+		}
+		
+	}
+	
+	public static int profGenTreeChild(GeneralTree t){
+		List<GeneralTree> liste = t.getChildren();
+		
+		if(t.getChildren().isEmpty())
+			return 1;
+		
+		else{
+			List<Integer> prof = new LinkedList<>();
+			int v = 0;
+			for(GeneralTree gt : liste){
+				if(!gt.getChildren().isEmpty())
+					v = (1+profGenTreeChild(gt));
+				else
+					v = 1;
+				prof.add(v);
+			}
+			
+			int base = 0;
+			for(Integer val : prof)
+				base = Math.max(base, val);
+			
+			return base;
+			
+		}
 	}
 
 	public static int profondeur(NoeudBinaire arbre){
@@ -72,16 +123,52 @@ public class Statistiques {
 			return (1+largeurDroite((NoeudBinaire)droite.getDroite()));
 	}
 	
+	public static int largeurGenTree(GeneralTree tree){
+		if(tree.getChildren().isEmpty())
+			return 1;
+		else{
+			GeneralTree first = tree.getChildren().get(0);
+			GeneralTree last = tree.getChildren().get(tree.getChildren().size()-1);
+			
+			return (1+largeurLeftGenTree(first)+largeurRightGenTree(last));
+		}
+	}
+	
+	public static int largeurLeftGenTree(GeneralTree left){
+		if(left.getChildren().isEmpty())
+			return 1;
+		else{
+			return (1+largeurLeftGenTree(left.getChildren().get(0)));
+		}
+	}
+	
+	public static int largeurRightGenTree(GeneralTree right){
+		if(right.getChildren().isEmpty())
+			return 1;
+		else{
+			return (1+largeurRightGenTree(right.getChildren().get(right.getChildren().size()-1)));
+		}
+	}
+	
 	
 
 	public static void main(String[] args) {
-		Noeud arbre = Arbre_recursif.recursiveTree(150, 2);
+		long before = System.currentTimeMillis();
+		Noeud arbre = Arbre_recursif.recursiveTree(40, 2);
+		long after = System.currentTimeMillis();
+		System.out.println("Execution time : "+(after-before)+" ms ("+((after-before)/1000.0)+" s)");
 		System.out.println(arbre);
 		int profondeur = profondeur(arbre);
-		System.out.println("profondeur arbre = "+profondeur);
+		System.out.println("profondeur arbre -> "+profondeur);
 		
 		int largeurArbre = largeurArbre(arbre);
-		System.out.println("largeur de l'arbre : "+largeurArbre);
+		System.out.println("largeur de l'arbre -> "+largeurArbre);
+		System.out.println();
+		GeneralTree tree1 = ArbreGeneraux.recursiveGeneralTree(16);
+		int prof = profondeurGenTree(tree1);
+		System.out.println(tree1);
+		System.out.println("profondeur arbre général -> "+prof);
+		System.out.println("largeur arbre général ->"+largeurGenTree(tree1));
 	}
 
 }
